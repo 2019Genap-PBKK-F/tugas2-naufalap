f41dcc5e31826d085bf9333be078f89b62150f23
naufalap <32903336+naufalap@users.noreply.github.com>
2020-02-26 10:34:53 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -10,5 +10,5 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(port, hostname, () => {
-  console.log(Server running at http://10.199.14.46:8002/);
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
